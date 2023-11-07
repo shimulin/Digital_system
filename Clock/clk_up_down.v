f275cd2056691f0509619017a6d8ea62e63fc87a
shimulin <114748690+shimulin@users.noreply.github.com>
2023-11-07 17:36:12 +0800

    Add files via upload

---
+++ Clock/clk_up_down.v
@@ -0,0 +1,24 @@
+module clk_up_down(clk, rst, up_down, OUT);
+
+input clk, rst, up_down;
+output reg[3:0] OUT;
+
+always @(posedge clk or negedge rst)
+begin
+	if(!rst) OUT <= 4'd0;
+	else
+	begin
+		if(up_down)
+		begin
+			if(OUT == 4'd15) OUT <= 4'd0;
+			else OUT <= OUT + 1;
+		end
+		else
+		begin
+			if(OUT == 4'd0) OUT <= 4'd15;
+			else OUT <= OUT - 1;
+		end
+	end
+end
+
+endmodule
